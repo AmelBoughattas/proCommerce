28510252b1a7bc925979c6742c144a821e0bc119
AmelBoughattas <amelbgh.misi@gmail.com>
2021-09-15 20:16:26 +0100

    changing to proxy

--- backend/helpers/db.js
+++ backend/helpers/db.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, });
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+       useUnifiedTopology: true, });
 
     console.log("MongoDB connection SUCCESS");
 
