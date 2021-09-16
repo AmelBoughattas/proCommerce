d7130a284d60eb722702826878953308ebbb537e
AmelBoughattas <amelbgh.misi@gmail.com>
2021-09-16 18:10:42 +0100

    changing to server page

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,16 @@ app.use("/api/products", require('./routes/productRoutes'))
 app.use("/api/command",require('./routes/commandRouter'))
 
 //setup for deployment
-app.use(express.static(path.join(__dirname,'../','frontend','build')));
+if(process.env.NODE_ENV === 'production'){
+    app.use('/',express.static('frontend/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'frontend/build/index.html'))
+    })
+}
+/* app.use(express.static(path.join(__dirname,'../','frontend','build')));
 app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname,'../','frontend','build','index.html'));
 });
-
+ */
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
