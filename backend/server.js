0987a22954593d4523ee8c4bc93278f465ad08fd
AmelBoughattas <amelbgh.misi@gmail.com>
2021-09-17 09:02:03 +0100

    inataller path

--- backend/server.js
+++ backend/server.js
@@ -36,11 +36,6 @@ app.use("/api/command",require('./routes/commandRouter'))
 app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname,'../','frontend','build','index.html'));
 });
-/* } */
-/* app.use(express.static(path.join(__dirname,'../','frontend','build')));
-app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname,'../','frontend','build','index.html'));
-});
- */
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
